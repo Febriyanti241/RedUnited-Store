42060d5cc6c367c700db4e3118ec5a951331c0e6
febriyanti241 <pebriy728@gmail.com>
2025-11-18 18:30:32 +0700

    Belum implemen my product

---
+++ authentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from authentication.views import login, register, create_product_flutter
+
+app_name = 'authentication'
+
+urlpatterns = [
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
+]
--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,8 @@ from main.views import register
 from main.views import login_user
 from main.views import logout_user
 from django.urls import path
-from main.views import show_main, create_product, show_product, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import show_main, create_product, show_product, show_xml, show_json, show_xml_by_id, show_json_by_id, proxy_image
+
 from main.views import edit_product
 from main.views import delete_product
 from main.views import delete_product_ajax
@@ -22,5 +23,6 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('product/<str:id>/edit', edit_product, name='edit_product'),
     path('product/<str:id>/delete', delete_product, name='delete_product'),
-    path('product/<str:id>/delete-ajax', delete_product_ajax, name='delete_product_ajax')   
+    path('product/<str:id>/delete-ajax', delete_product_ajax, name='delete_product_ajax'),
+    path('proxy-image/', proxy_image, name='proxy_image'),
 ]
